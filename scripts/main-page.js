6d6d88cf7ebc833225443952bf759d5046609235
Yana Kylymysta <zozulyaweb@gmail.com>
2024-06-23 23:38:47 +0300

    Addinf animation 1

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -1,9 +1,10 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    const mainSwiper = new Swiper(".main-banner__slider", {
-        loop:true,
+ //MAIN SLIDER initialization
+ const mainSwiper = new Swiper(".main-banner__slider", {
+            loop:true,
             autoplay: {
-                delay: 5000,
+                delay: 6000,
             },
             effect: 'fade',
             enabled: true,
@@ -20,22 +21,119 @@ document.addEventListener('DOMContentLoaded', function () {
                 },
             },
         });
-            // swiper.on('slideChange', function () {
-            //     gsap.to(swiper.slides[swiper.previousIndex], { opacity: 0.8, scale: 1.3 });
-            //     gsap.to(swiper.slides[swiper.activeIndex], { scale: 1, opacity: 1 });
-            //      gsap.to(swiper.slides[swiper.previousIndex], { opacity: 0.8, scale: 1.3 });
-                
-            //     swiper.slides[swiper.previousIndex].animation.pause(0);
-            //     swiper.slides[swiper.activeIndex].animation.restart();
-            //     swiper.slides.forEach((slide, index) => {
-            //         if (index === swiper.activeIndex) {
-            //             slide.classList.add('active-slide');
-            //         } else {
-            //             slide.classList.remove('active-slide');
-            //         }
-            //     });
-            // });
-     
+    //GSAP ANIMATION HERO SECTION
+    TweenMax.from('.tweenmax-banner', 0.8, {
+            opacity: 0,
+            delay: 0.3,
+            scale: 0.94,
+            easy: Expo.easyInOut
+     })
+    TweenMax.fromTo('.tweenmax-hero', 0.4, {
+            opacity: 0,
+            y: 10,
+        },
+        {
+            opacity: 1,
+            delay: 1.1,
+            y: 0,
+            easy: Expo.easyInOut
+        })
+ 
+    TweenMax.staggerFrom('.menu__item', 0.4, {
+        autoAlpha: 0,
+        delay: 1.1,
+        y: 10,
+        easy: Power3.easyInOut
+    }, 0.1)
+   TweenMax.staggerFrom('.swiper-pagination-bullet', 0.4, {
+        autoAlpha: 0,
+        delay: 1.1,
+        y: 10,
+        easy: Power3.easyInOut
+    }, 0.1)
+
+    TweenMax.from('.main-banner__left-container', 1, {
+            opacity: 0,
+            y: '100%',
+            delay: 1.4,
+            easy: Expo.easyInOut
+     })
+
+    // GSAP ANIMATION ABOUT US SECTION
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from('.about__img-left', {
+            y: '-20%',
+            scale: 1.05,
+            opacity: 0,
+            duration: 0.8,
+            scrollTrigger: {
+                trigger: '.about',
+                start: 'top bottom',
+                end: 'bottom top',
+                toggleActions: 'restart none  none  none '
+            }
+        });
+     gsap.from('.about__img-right', {
+            y: '20%',
+            scale: 1.05,
+            opacity: 0,
+            duration: 0.8,
+            scrollTrigger: {
+                trigger: '.about',
+                start: 'top bottom',
+                end: 'bottom top',
+                toggleActions: 'restart none  none  none '
+            }
+        });
+    gsap.from('.about__desc', {
+            x: '30%',
+            opacity: 0,
+            duration: 0.8,
+            scrollTrigger: {
+                trigger: '.about',
+                start: 'top bottom',
+                end: 'bottom top',
+                toggleActions: 'restart none  none  none '
+            }
+        });
+
+    const numberDisplays = document.querySelectorAll('.amount__numb');
+    numberDisplays.forEach(num => {
+        const targetValue = +(num.dataset.num);
+        gsap.to(num, {
+            duration: 1.6,
+            ease: "power4.out",
+            scrollTrigger: {
+                trigger: '.amounts__container',
+                start: 'top bottom',
+                end: 'bottom top',
+                toggleActions: 'restart none  none  none '
+            },
+            innerHTML: targetValue,
+            onUpdate: function () {
+                num.innerHTML = Math.round(num.innerHTML);
+            },
+            onComplete: function () {
+                num.innerHTML += "+"; 
+            }
+        });
+    });
+
+    //GSAP ANIMATION SERVICES SECTION
+    gsap.from('.service', {
+        opacity: 0,
+        y: 80,
+        duration: 0.8,
+        stagger: 0.2,
+        scrollTrigger: {
+            trigger: '.service',
+            start: 'top bottom',
+            end: 'bottom top',
+            toggleActions: 'restart none  none  none '
+        }
+    });
+   //MANUFACTURE SLIDER initialization
     const manufacturerSwiper = new Swiper(".manufacturers__slider", {
         spaceBetween: 16,
         slidesPerView: 1,
@@ -113,142 +211,79 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-//ANIMATION
-gsap.registerPlugin(ScrollTrigger);
 
-//ABOUT US BLOCK
-gsap.to('.about__img', {
-        y: 0,
-        scale: 1,
-        opacity: 1,
-        duration: 1,
-        scrollTrigger: {
-            trigger: '.about',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        }
-    });
+// //COMPANIES BLOCK
+// if(windowWidth < 1024) {
+//     gsap.from('.companies__logo', {
+//         opacity: 0,
+//         x: 200,
+//         duration: 1,
+//         scrollTrigger: {
+//             trigger: '.companies__logo',
+//             start: 'top bottom',
+//             end: 'bottom top',
+//             toggleActions: 'restart none  none  none '
+//         }
+//     });
+// } else {
+//     gsap.from('.companies__desc-left', {
+//         opacity: 0,
+//         x: -200,
+//         duration: 1,
+//         scrollTrigger: {
+//             trigger: '.companies',
+//             start: 'top bottom',
+//             end: 'bottom top',
+//             toggleActions: 'restart none  none  none '
+//         }
+//     });
+//     gsap.from('.companies__logo', {
+//         opacity: 0,
+//         x: 200,
+//         duration: 1,
+//         scrollTrigger: {
+//             trigger: '.companies',
+//             start: 'top bottom',
+//             end: 'bottom top',
+//             toggleActions: 'restart none  none  none '
+//         }
+//     });
+// }
 
-gsap.to('.about__desc', {
-        x: 0,
-        opacity: 1,
-        duration: 1,
-        scrollTrigger: {
-            trigger: '.about',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        }
-    });
-
-const numberDisplays = document.querySelectorAll('.amount__numb');
-numberDisplays.forEach(num => {
-    const targetValue = +(num.dataset.num);
-    gsap.to(num, {
-        duration: 3,
-        ease: "power4.out",
-        scrollTrigger: {
-            trigger: '.amounts__container',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        },
-        innerHTML: targetValue,
-        onUpdate: function () {
-            num.innerHTML = Math.round(num.innerHTML);
-        },
-        onComplete: function () {
-            num.innerHTML += "+"; 
-        }
-    });
-});
-//SERVICES BLOCK
-gsap.from('.service', {
-    opacity: 0,
-    y: 150,
-    duration: 0.8,
-    stagger: 0.3,
-    scrollTrigger: {
-        trigger: '.service',
-        start: 'top bottom',
-        end: 'bottom top',
-        toggleActions: 'restart none  none  none '
-    }
-});
-
-//COMPANIES BLOCK
-if(windowWidth < 1024) {
-    gsap.from('.companies__logo', {
-        opacity: 0,
-        x: 200,
-        duration: 1,
-        scrollTrigger: {
-            trigger: '.companies__logo',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        }
-    });
-} else {
-    gsap.from('.companies__desc-left', {
-        opacity: 0,
-        x: -200,
-        duration: 1,
-        scrollTrigger: {
-            trigger: '.companies',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        }
-    });
-    gsap.from('.companies__logo', {
-        opacity: 0,
-        x: 200,
-        duration: 1,
-        scrollTrigger: {
-            trigger: '.companies',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        }
-    });
-}
-
-//SLIDER BLOCK
-if(windowWidth < 1200) {
-    gsap.from('.animate-slider', {
-        opacity: 0,
-        y: 150,
-        duration: 1,
-        scrollTrigger: {
-            trigger: '.animate-slide',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        }
-    });
-} else {
-    gsap.fromTo('.animate-slide', 
-      { opacity: 0, y: 200 },
-      { opacity: 1,
-        y: 0,
-        duration: 0.8,
-        stagger: 0.3,
-        scrollTrigger: {
-            trigger: '.animate-wrapper',
-            start: 'top bottom',
-            end: 'bottom top',
-            toggleActions: 'restart none  none  none '
-        }
-    });
-}
+// //SLIDER BLOCK
+// if(windowWidth < 1200) {
+//     gsap.from('.animate-slider', {
+//         opacity: 0,
+//         y: 150,
+//         duration: 1,
+//         scrollTrigger: {
+//             trigger: '.animate-slide',
+//             start: 'top bottom',
+//             end: 'bottom top',
+//             toggleActions: 'restart none  none  none '
+//         }
+//     });
+// } else {
+//     gsap.fromTo('.animate-slide', 
+//       { opacity: 0, y: 200 },
+//       { opacity: 1,
+//         y: 0,
+//         duration: 0.8,
+//         stagger: 0.3,
+//         scrollTrigger: {
+//             trigger: '.animate-wrapper',
+//             start: 'top bottom',
+//             end: 'bottom top',
+//             toggleActions: 'restart none  none  none '
+//         }
+//     });
+// }
 
 //FORM
 gsap.from('.cta__block', {
     opacity: 0,
-    y: 150,
-    duration: 0.8,
+    scale: 0.96,
+    duration: 0.6,
     scrollTrigger: {
         trigger: '.cta__block',
         start: '30% bottom',
