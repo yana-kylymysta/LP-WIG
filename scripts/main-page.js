9d39336307c6706f6b7370d1ba23439147622b27
Yana Kylymysta <zozulyaweb@gmail.com>
2025-01-13 13:28:30 +0200

    added elevator cards

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -184,35 +184,6 @@ if(windowWidth < 1024) {
         }
     });
 
-//MANUFACTURE SLIDER initialization
-    const manufacturerSwiper = new Swiper(".manufacturers__slider", {
-        spaceBetween: 16,
-        slidesPerView: 1,
-        grabCursor:true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            700: {
-                slidesPerView: 2,
-                spaceBetween: 24,
-            },
-            1024: {
-                slidesPerView: 2,
-                pagination: false,
-                navigation: {
-                    nextEl: ".swiper-button-next",
-                    prevEl: ".swiper-button-prev",
-                },
-            },
-            1200: {
-                slidesPerView: 3,
-                pagination: false
-            }
-            }
-    });
-
 
 //GSAP ANIMATION COMPANIES BLOCK
     if(windowWidth < 1024) {
